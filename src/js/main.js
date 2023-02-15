db7435f23fbe397699cfd809b16a057f1211ab1e
Ksenny <ksenny_h@mail.ru>
2023-02-15 21:37:43 +0300

    page product-card: tabs script

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
 import swiper from "./modules/swiper.js";
-swiper();
+import tabs from "./modules/tabs.js";
+//swiper();
+
+const colorBtns = document.querySelectorAll('.js-color-btn');
+const colorImg = document.querySelectorAll('.js-img');
+
+colorBtns.forEach(el => {
+    el.addEventListener("click", (e) => {
+        let currentColorBtn = e.currentTarget;
+        let tabId = currentColorBtn.getAttribute('data-color');
+        let currentColorImg = document.querySelector(tabId);
+
+        if ( !currentColorBtn.classList.contains('active') ) {
+            colorBtns.forEach(el => {
+                el.classList.remove('active');
+            });
+
+            colorImg.forEach(el => {
+                el.classList.remove('active');
+            });
+        }
+
+        currentColorBtn.classList.add('active');
+        currentColorImg.classList.add('active');
+    })
+})
+document.querySelector('.js-color-btn').click();
+
