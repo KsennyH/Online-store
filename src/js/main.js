7d0e2923da41d74ab35a38b8b05dc900f00083ed
Ksenny <ksenny_h@mail.ru>
2023-02-23 17:51:59 +0300

    motoservices adaptive and script

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,37 @@ import mobileNavigation from "./modules/mobileNavigation.js";
 import spoilerFooter from "./modules/spoilerFooter.js";
 import spoilerFeature from "./modules/spoilerFeature.js";
 import mobileFilter from "./modules/mobileFilter.js";
-tabs();
-features();
+//tabs();
+//features();
 mobileNavigation();
-spoilerFeature();
-spoilerFooter();
-mobileFilter();
+//spoilerFeature();
+//spoilerFooter();
+//mobileFilter();
+
+const btnTab = document.querySelectorAll('.js-btn-tab');
+const tabItem = document.querySelectorAll('.js-tab');
+
+btnTab.forEach(el => {
+    el.addEventListener('click', (e) => {
+        let currentTabBtn = e.currentTarget;
+        let idTab = currentTabBtn.getAttribute('data-service');
+        let currentTabItem = document.querySelector(idTab);
+
+        if ( !currentTabBtn.classList.contains('active') ) {
+            btnTab.forEach(el => {
+                el.classList.remove('active');
+            });
+
+            tabItem.forEach(el => {
+                el.classList.remove('active');
+            });
+        }
+
+        currentTabBtn.classList.add('active');
+        currentTabItem.classList.add('active');
+    })
+})
+document.querySelector('.js-btn-tab').click();
 
 
 
