0ba1566bbe29a4ce94525479a6865f173dbc6fd7
Ksenny <ksenny_h@mail.ru>
2023-02-25 20:53:28 +0300

    login script

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,17 @@ mobileNavigation();
 spoilerFooter();
 
 const loginBtn = document.querySelector('.js-login');
+const loginPopup = document.querySelector('.js-login-popup');
+const loginClose = document.querySelector('.login__close');
 
 loginBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    
+    loginPopup.classList.add('active');
+})
+
+loginClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    loginPopup.classList.remove('active');
 })
 
 
