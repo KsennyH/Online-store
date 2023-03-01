4bfc8eff2b4a0d66df14b236f8852314367c6171
Ksenny <ksenny_h@mail.ru>
2023-03-01 22:29:26 +0300

    script counter

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,26 @@ document.addEventListener('click', (e) => {
     }
 })
 
+window.addEventListener('click', (e) => {
+
+    let inputCounter;
+
+    if(e.target.dataset.button === "up" || e.target.dataset.button === "down" ) {
+        const counter = e.target.closest('.counter');
+        inputCounter = counter.querySelector('[data-counter]');
+    }
+
+    if( e.target.dataset.button === "up" ) {
+        inputCounter.value = ++inputCounter.value;
+    }
+
+    if( e.target.dataset.button === "down" ) {
+        if( inputCounter.value > 1 ) {
+            inputCounter.value = --inputCounter.value;
+        }
+    }
+});
+
 
 
 
