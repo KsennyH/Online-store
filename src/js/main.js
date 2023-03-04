9a923300d88954a7a32ebc91648764a7d97f3206
Ksenny <ksenny_h@mail.ru>
2023-03-04 11:52:07 +0300

    script: add product to cart

--- src/js/main.js
+++ src/js/main.js
@@ -50,16 +50,35 @@ window.addEventListener('click', (e) => {
 });
 
 //Добавление в корзину
+const cartProducts = document.querySelector('.cart-products');
 window.addEventListener('click', (e) => {
     if(e.target.hasAttribute('data-cart')) {
         const card = e.target.closest('.product-card');
         
         const dataProduct = {
+            id: card.dataset.id,
             imgSrc: card.querySelector('.js-img-card').getAttribute('src'),
             title: card.querySelector('.product-card__title').innerText,
             price: card.querySelector('.product-card__price').innerText
-        }
-        console.log(dataProduct);
+        };
+
+        const cardTemplate = `<div class="cart-product" data-id="${dataProduct.id}"> 
+            <div class="cart-product__img"> <img src="${dataProduct.imgSrc}" alt="${dataProduct.title}"/></div>
+            <div class="cart-product__title">${dataProduct.title}</div>
+            <div class="cart-product__counter">
+            <div class="counter"> 
+                <button class="counter__btn counter__btn--down" type="button" data-button="down">-</button>
+                <input class="counter__input" type="number" data-counter="data-counter" value="1"/>
+                <button class="counter__btn counter__btn--up" type="button" data-button="up">+</button>
+            </div>
+            </div>
+            <div class="cart-product__price"> 
+            <p>${dataProduct.price}</p>
+            </div>
+            <button class="cart-product__deleate btn-close" type="button"><span> </span></button>
+        </div>`
+
+        cartProducts.insertAdjacentHTML('beforeend', cardTemplate);
     }
 })
 
