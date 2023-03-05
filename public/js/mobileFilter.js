74f340086cfa5dbac2af27ca6966a8ab612a3daf
Ksenny <ksenny_h@mail.ru>
2023-03-05 16:47:58 +0300

    refactoring main.js

--- public/js/mobileFilter.js
+++ public/js/mobileFilter.js
@@ -1,10 +1,6 @@
 const mobileFilterBtn = document.querySelector('.js-filter-btn');
 const mobileFilterForm = document.querySelector('.js-filter');
 
-const mobileFilter = () => {
-    mobileFilterBtn.addEventListener('click', () => {
-        mobileFilterForm.classList.toggle('active');
-    });
-}
-
-export default mobileFilter;
+mobileFilterBtn.addEventListener('click', () => {
+    mobileFilterForm.classList.toggle('active');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -4,42 +4,14 @@ import spoilerFooter from "./modules/spoilerFooter.js";
 import counter from "./modules/counter.js";
 import deleateFromCart from "./modules/deleateFromCart.js";
 import addToCart from "./modules/addToCart.js";
+import loginModal from "./modules/loginModal.js";
 deleateFromCart();
 counter();
 addToCart();
+loginModal();
 mobileNavigation();
 spoilerFooter();
 
-
-window.addEventListener('click', (e) => {
-    if(e.target.contains('.js-login')) {
-        const loginBtn = document.querySelector('.js-login');
-    }
-})
-
-const loginBtn = document.querySelector('.js-login');
-const popup = document.querySelector('.js-popup');
-const login = document.querySelector('.js-login-popup');
-const loginClose = document.querySelector('.login__close');
-
-/*loginBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    popup.classList.add('active');
-    login.classList.add('active');
-})
-
-loginClose.addEventListener('click', (e) => {
-    e.preventDefault();
-    popup.classList.remove('active');
-    login.classList.remove('active');
-})
-
-document.addEventListener('click', (e) => {
-    if(e.target === popup) {
-        login.classList.remove('active');
-        popup.classList.remove('active');
-    }
-})*/
 
 
 
