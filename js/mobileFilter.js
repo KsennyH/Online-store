c6b9617ebb91c33fa92d82d54fd7c0c9875b4ebc
Ksenny <ksenny_h@mail.ru>
2023-04-29 13:41:47 +0300

    js refactoring-3

--- js/mobileFilter.js
+++
@@ -1,6 +0,0 @@
-const mobileFilterBtn = document.querySelector('.js-filter-btn');
-const mobileFilterForm = document.querySelector('.js-filter');
-
-mobileFilterBtn.addEventListener('click', () => {
-    mobileFilterForm.classList.toggle('active');
-});
--- js/spoilerFeature.js
+++
@@ -1,7 +0,0 @@
-const spoilers = document.querySelectorAll("[data-name='spoiler-feature']");
-
-spoilers.forEach(el => {
-    el.addEventListener('click', function() {
-        this.nextElementSibling.classList.toggle('spoiler-feature');
-    });
-});
